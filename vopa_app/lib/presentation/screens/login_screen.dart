8230aa83443d95014547436541456dcce32b5a16
francisojeah <ojeahfrancis@gmail.com>
2024-09-29 20:56:32 +0100

    login screen working with firebase

---
+++ vopa_app/lib/presentation/screens/login_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> _loginUser() async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      Navigator.pushNamed(context, AppConstants.homeScreenRoute);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(controller: emailController, decoration: InputDecoration(hintText: 'Email')),
+            TextField(controller: passwordController, decoration: InputDecoration(hintText: 'Password')),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _loginUser, child: Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+}
