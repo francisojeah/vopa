3c22f9e00e927841788e13441772a64da7d7afad
francisojeah <ojeahfrancis@gmail.com>
2024-09-29 20:59:23 +0100

    Sign-Up screens working properly with Firebase

---
+++ vopa_app/lib/presentation/screens/signup_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_screen.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> _registerUser() async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      // You can also add additional user info to Firestore if needed
+
+      // Navigate to the home screen after successful signup
+      Navigator.pushReplacementNamed(context, AppConstants.homeScreenRoute);
+    } catch (e) {
+      print(e); // Handle registration errors here
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sign Up')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(hintText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(hintText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _registerUser,
+              child: Text('Sign Up'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Go back to login screen
+              },
+              child: Text('Already have an account? Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
