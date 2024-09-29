a457fc52b4c05da6bd6b8c77971b36ac597e2bde
francisojeah <ojeahfrancis@gmail.com>
2024-09-29 20:50:46 +0100

    define constants for routes, api endpoints

---
+++ vopa_app/lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String apiBaseUrl = 'https://api-url.com';
+  static const String homeScreenRoute = '/home';
+  static const String loginScreenRoute = '/login';
+  static const String voiceCommandScreenRoute = '/voice-command';
+}
